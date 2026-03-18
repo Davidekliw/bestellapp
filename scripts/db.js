472b0087ca3c6f805db4c86a6a541ea331d84be1
David <david-wilke@live.com>
2026-03-18 13:38:11 +0100

    feat: add initial data structure in db.js

---
+++ scripts/db.js
@@ -0,0 +1,107 @@
+const BURGERS = [
+    {
+        "imgPath": "assets/img/food_small_8.jpg",
+        "title": "Veggie mushroom black burger",
+        "ingredient": ["Mixed green salad", "Tomatoes", "Edamame", "Mushrooms"],
+        "price": 16.90
+    },
+    {
+        "imgPath": "assets/img/food_small_5.jpg",
+        "title": "All meat burger",
+        "ingredient": ["Beef", "Bacon", "Dill pickles", "Smoked cheese", "Ketchup", "BBQ souse"],
+        "price": 15.90
+    },
+    {
+        "imgPath": "assets/img/food_small_12.jpg",
+        "title": "Beef red burger",
+        "ingredient": ["Beef", "Cheese", "Tomatoes", "Lettuce", "Onion"],
+        "price": 14.90
+    },
+    {
+        "imgPath": "assets/img/food_small_7.jpg",
+        "title": "Big chicken burger",
+        "ingredient": ["Chicken", "Cheese", "Tomatoes", "Lettuce", "Onion", "Bell pepper"],
+        "price": 15.90
+    }
+]
+
+const PIZZA = [
+    {
+        "imgPath": "assets/img/food_small_2.jpg",
+        "title": "Pizza Margherita",
+        "ingredient": ["Tomato Sauce", "Mozzarella"],
+        "price": 11.90
+    },
+    {
+        "imgPath": "assets/img/food_small_14.jpg",
+        "title": "Pizza Chorizo",
+        "ingredient": ["Tomato slices", "Mozzarella", "Chorizo"],
+        "price": 13.90
+    },
+    {
+        "imgPath": "assets/img/food_small_1.jpg",
+        "title": "Funghi",
+        "ingredient": ["Red onion", "Olives", "Button Mushrooms", "Mozzarella"],
+        "price": 12.90
+    },
+    {
+        "imgPath": "assets/img/food_small_11.jpg",
+        "title": "Quattro Formaggi with Chicken",
+        "ingredient": ["Chicken", "Mozzarella", "Gorgonzola", "Fontina", "Parmigiano Reggiano"],
+        "price": 15.90
+    }
+]
+
+const SALADS = [
+    {
+        "imgPath": "assets/img/food_small_9.jpg",
+        "title": "Warm beef arugula salad",
+        "ingredient": ["Beef", "Arugula", "Field salad", "Greek feta", "Cherry tomatoes", "Sun-dried tomatoes", "Balsamic-vinegar dressing"],
+        "price": 16.90
+    },
+    {
+        "imgPath": "assets/img/food_small_4.jpg",
+        "title": "Mini green Salad",
+        "ingredient": ["Green salad", "Cucumber", "Carrots", "Parsley", "Radishes"],
+        "price": 7.90
+    },
+    {
+        "imgPath": "assets/img/food_small_6.jpg",
+        "title": "Green Salad with sea food",
+        "ingredient": ["Mixed greens", "Cherry tomatoes", "Red onion", "Mussels", "Squid rings", "Shrimp", "Dijon mustard-lemon dressing with dill"],
+        "price": 16.90
+    },
+    {
+        "imgPath": "assets/img/food_small_10.jpg",
+        "title": "Vegan green salad with tofu",
+        "ingredient": ["Green salad", "Cherry tomatoes", "Cucumber", "Baby spinach", "Edamame", "Radishes", "Bittercress", "Tofu", "Peanuts"],
+        "price": 14.90
+    }
+]
+
+const MENUHEADLINES = [
+    {
+        "imgPath": "assets/img/food_small_9.jpg",
+        "title": "Warm beef arugula salad",
+        "ingredient": ["Beef", "Arugula", "Field salad", "Greek feta", "Cherry tomatoes", "Sun-dried tomatoes", "Balsamic-vinegar dressing"],
+        "price": 16.90
+    },
+    {
+        "imgPath": "assets/img/food_small_4.jpg",
+        "title": "Mini green Salad",
+        "ingredient": ["Green salad", "Cucumber", "Carrots", "Parsley", "Radishes"],
+        "price": 7.90
+    },
+    {
+        "imgPath": "assets/img/food_small_6.jpg",
+        "title": "Green Salad with sea food",
+        "ingredient": ["Mixed greens", "Cherry tomatoes", "Red onion", "Mussels", "Squid rings", "Shrimp", "Dijon mustard-lemon dressing with dill"],
+        "price": 16.90
+    },
+    {
+        "imgPath": "assets/img/food_small_10.jpg",
+        "title": "Vegan green salad with tofu",
+        "ingredient": ["Green salad", "Cherry tomatoes", "Cucumber", "Baby spinach", "Edamame", "Radishes", "Bittercress", "Tofu", "Peanuts"],
+        "price": 14.90
+    }
+]
