886fd4bbe940155831c6349ae3be33fad5f12e8d
David <david-wilke@live.com>
2026-03-19 01:48:16 +0100

    feat: refactor render function and add category headlines template

--- script.js
+++ script.js
@@ -7,20 +7,26 @@ function toogleBurgerMenu() {
 
 
 function renderAllMeals() {
-    getMeals(burgers);
-    getMeals(pizza);
-    getMeals(salads);
+    getMeals(BURGERS, "burgers");
+    getMeals(PIZZA, "pizza");
+    getMeals(SALADS, "salads");
 }
 
-function getMeals(type) {
-    const category = type;
+function getMeals(category, type) {
     let box = document.getElementById('meal');
-    box.innerHTML += '';
+    box.innerHTML += getMealsHeadlines(type);
     for (let i = 0; i < category.length; i++) {
         const path = category[i]['imgPath'];
         const title = category[i]['title'];
         const ingredient = category[i]['ingredient'];
-        const price = category[i]['price'];
+        let price = category[i]['price'];
+        price = changePriceFormat(price);
         box.innerHTML += generateMealsTemplate(path, title, ingredient, price, i)
     }
+}
+
+function getMealsHeadlines(categoryHeadings) {
+    const path = MENUHEADLINES[categoryHeadings]['imgPath'];
+    const title = MENUHEADLINES[categoryHeadings]['title'];
+    return generateMealsSectionHeadlines(path, title);
 }
