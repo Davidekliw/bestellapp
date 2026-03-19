9b200641058ea35725611a8dabcb0f4ef72dfe36
David <david-wilke@live.com>
2026-03-19 01:43:49 +0100

    feat: refactor meal data structure and update content

--- scripts/db.js
+++ scripts/db.js
@@ -79,29 +79,17 @@ const SALADS = [
     }
 ]
 
-const MENUHEADLINES = [
-    {
-        "imgPath": "assets/img/food_small_9.jpg",
-        "title": "Warm beef arugula salad",
-        "ingredient": ["Beef", "Arugula", "Field salad", "Greek feta", "Cherry tomatoes", "Sun-dried tomatoes", "Balsamic-vinegar dressing"],
-        "price": 16.90
+const MENUHEADLINES = {
+    "burgers": {
+        "imgPath": "assets/img/burger.png",
+        "title": "Burger & Sandwiches"
     },
-    {
-        "imgPath": "assets/img/food_small_4.jpg",
-        "title": "Mini green Salad",
-        "ingredient": ["Green salad", "Cucumber", "Carrots", "Parsley", "Radishes"],
-        "price": 7.90
+    "pizza": {
+        "imgPath": "assets/img/pizza.png",
+        "title": "Pizza (30cm)"
     },
-    {
-        "imgPath": "assets/img/food_small_6.jpg",
-        "title": "Green Salad with sea food",
-        "ingredient": ["Mixed greens", "Cherry tomatoes", "Red onion", "Mussels", "Squid rings", "Shrimp", "Dijon mustard-lemon dressing with dill"],
-        "price": 16.90
+    "salads": {
+        "imgPath": "assets/img/salad.png",
+        "title": "Salad"
     },
-    {
-        "imgPath": "assets/img/food_small_10.jpg",
-        "title": "Vegan green salad with tofu",
-        "ingredient": ["Green salad", "Cherry tomatoes", "Cucumber", "Baby spinach", "Edamame", "Radishes", "Bittercress", "Tofu", "Peanuts"],
-        "price": 14.90
-    }
-]
+}
